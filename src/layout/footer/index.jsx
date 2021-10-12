97215aa3c8199c7d440ce4ddbb8dbec4a2fbdc8b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 19:30:02 +0700

    edit footer

--- src/layout/footer/index.jsx
+++ src/layout/footer/index.jsx
@@ -4,21 +4,33 @@ import { BMKAFullIcon } from '@assets'
 import { ComponentWrapper } from '@components'
 
 import FooterText from './text'
-import { MenuSection, AddressSection, ContactSection } from './section'
+import FooterTitle from './title'
+import { AddressSection, ContactSection } from './section'
 
 const Footer = () => {
     return (
         <footer className='py-10 bg-bmka-shade-dark-purple'>
             <ComponentWrapper>
                 <div className='flex justify-between'>
-                    <div className='flex flex-col gap-2'>
+                    <div className='max-w-sm flex flex-col gap-4'>
+                        <FooterTitle>
+                            Platform Aktivis Salman ITB (BMKA)
+                        </FooterTitle>
                         <div className='w-32'>
                             <BMKAFullIcon />
                         </div>
-                        <FooterText>Copyright BMKA Salman 2021</FooterText>
+                        <FooterText>
+                            Portal Aktivis Salman yang dikelola BMKA (Bidang
+                            Kemahasiswaan, Kaderisasi dan Alumni) Salman yang
+                            berfungsi sebagai pusat pendaftaran kegiatan di
+                            @kaderisasisalman. Program pembinaan dalam rangka
+                            membentuk kader teladan untuk membangun Indonesia.
+                        </FooterText>
+                        <p className='mt-4 text-white text-xs font-bold'>
+                            Copyright BMKA Salman 2021
+                        </p>
                     </div>
-                    <div className='flex gap-6'>
-                        <MenuSection />
+                    <div className='flex gap-10'>
                         <ContactSection />
                         <AddressSection />
                     </div>
