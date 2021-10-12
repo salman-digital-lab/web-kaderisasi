14b371c458825dbbac8c58af30b93f525ab2a1cf
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 14:51:25 +0700

    layouting navigation

--- src/layout/navigation/index.jsx
+++ src/layout/navigation/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
+
+import { ComponentWrapper } from '@components'
 
 const Navigation = () => {
     return (
-        <nav>
-            <p>Navigation</p>
+        <nav className='fixed left-0 top-0 w-full py-2 px-4 bg-bmka-primary-blue'>
+            <ComponentWrapper>
+                <div className='flex justify-between'>
+                    <div>
+                        <p>Logo Here</p>
+                    </div>
+                    <div>
+                        <p>Link</p>
+                    </div>
+                    <div>
+                        <p>User Auth</p>
+                    </div>
+                </div>
+            </ComponentWrapper>
         </nav>
     )
 }
