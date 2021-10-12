2380ab56f5d1669a9ba870c45f5aeeb8ef23b5e1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 14:22:38 +0700

    updating tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ const config = {
     purge: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
+            colors: {
+                'bmka-primary-blue': '#1F99CC',
+
+                'bmka-accent-blue': '#8DBCDD',
+                'bmka-accent-orange': '#FF7B40',
+
+                'bmka-shade-white': '#F7F5F5',
+                'bmka-shade-dark-purple': '#25223C',
+            },
             fontFamily: {
                 sans: ['Roboto', ...defaultTheme.fontFamily.sans],
             },
