3ec1c6d12805eafb3e9bfc709f46523da01cc26b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 14:18:03 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const config = {
+    darkMode: false,
     purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+            },
             gridTemplateRows: {
                 pageWrapper: '1fr auto;',
             },
@@ -13,3 +18,5 @@ module.exports = {
     },
     plugins: [],
 }
+
+module.exports = config
