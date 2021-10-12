b24ad62ffc18af7fbd9be404a978ab66745a6ebb
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 19:21:01 +0700

    implement all footer section

--- src/layout/footer/index.jsx
+++ src/layout/footer/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { BMKAFullIcon } from '@assets'
+import { ComponentWrapper } from '@components'
+
+import FooterText from './text'
+import { MenuSection, AddressSection, ContactSection } from './section'
+
 const Footer = () => {
     return (
-        <footer>
-            <p>footer</p>
+        <footer className='py-10 bg-bmka-shade-dark-purple'>
+            <ComponentWrapper>
+                <div className='flex justify-between'>
+                    <div className='flex flex-col gap-2'>
+                        <div className='w-32'>
+                            <BMKAFullIcon />
+                        </div>
+                        <FooterText>Copyright BMKA Salman 2021</FooterText>
+                    </div>
+                    <div className='flex gap-6'>
+                        <MenuSection />
+                        <ContactSection />
+                        <AddressSection />
+                    </div>
+                </div>
+            </ComponentWrapper>
         </footer>
     )
 }
