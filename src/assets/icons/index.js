c28c2e1b5f5c1dc4cdfdb7b3faba30c0e1b077a1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-12 19:01:07 +0700

    create line icon and exporting it

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,7 @@
 import MailIcon from './mail'
+import LineIcon from './line'
 import PhoneIcon from './phone'
 import BMKAFullIcon from './BMKAFull'
 import InstagramIcon from './instagram'
 
-export { MailIcon, PhoneIcon, BMKAFullIcon, InstagramIcon }
+export { MailIcon, LineIcon, PhoneIcon, BMKAFullIcon, InstagramIcon }
