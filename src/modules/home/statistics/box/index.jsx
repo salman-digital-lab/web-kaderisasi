77891deb71312c37739b11b267312e3dcf033f1f
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-13 19:02:33 +0700

    fix bold

--- src/modules/home/statistics/box/index.jsx
+++ src/modules/home/statistics/box/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = ({ number, title }) => {
     return (
         <div className='text-center p-8'>
-            <p className='text-3xl text-bmka-accent-orange font-bold'>
+            <p className='text-3xl text-bmka-accent-orange font-extrabold'>
                 {number}
             </p>
             <p>{title}</p>
