b56d061e120d2d75f909763e2e122c8f51503683
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-14 17:01:43 +0700

    fix card carousell

--- src/components/cardCarousell/index.jsx
+++ src/components/cardCarousell/index.jsx
@@ -2,13 +2,33 @@ import React from 'react'
 
 import { ComponentWrapper } from '@components'
 
-const CardCarousell = ({ children }) => {
+const CardCarousell = ({ title, children }) => {
     return (
-        <div>
-            <ComponentWrapper>
-                <div>{children}</div>
-            </ComponentWrapper>
-        </div>
+        <>
+            <div className='w-full grid gap-6'>
+                <ComponentWrapper>
+                    <div>
+                        <h2 className='font-bold'>{title}</h2>
+                    </div>
+                </ComponentWrapper>
+                <div
+                    className='w-full overflow-x-scroll'
+                    style={{
+                        scrollBehavior: 'smooth',
+                        scrollbarWidth: 'none',
+                    }}
+                >
+                    <ComponentWrapper>
+                        <div className='w-min flex gap-6'>{children}</div>
+                    </ComponentWrapper>
+                </div>
+            </div>
+            <style jsx>{`
+                ::-webkit-scrollbar {
+                    display: none;
+                }
+            `}</style>
+        </>
     )
 }
 
