d54c0f20bf988b96478aa782711bc6f4451dafef
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-14 17:38:01 +0700

    fix carousell

--- src/components/cardCarousell/index.jsx
+++ src/components/cardCarousell/index.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useRef, useEffect, Children } from 'react'
 
 import { ComponentWrapper } from '@components'
 
+import Header from './header'
+
 const CardCarousell = ({ title, children }) => {
+    const CardCarousellBoxRef = useRef()
+    const CardCarousellContentRef = useRef()
+
+    const getChildrenLength = Children.toArray(children).length
+
+    const getSizePerCard = () => {
+        const sizePerCard =
+            CardCarousellBoxRef.current.scrollWidth / getChildrenLength
+
+        return sizePerCard
+    }
+
+    const getCurrentPosition = () => {
+        const currentPositions = CardCarousellContentRef.current.scrollLeft
+
+        return currentPositions
+    }
+
+    const previousOnClickHandler = () => {
+        CardCarousellContentRef.current.scrollLeft =
+            getCurrentPosition() - getSizePerCard()
+    }
+
+    const nextOnClickHandler = () => {
+        CardCarousellContentRef.current.scrollLeft =
+            getCurrentPosition() + getSizePerCard()
+    }
+
+    useEffect(() => {
+        if (CardCarousellContentRef.current) {
+            CardCarousellContentRef.current.scrollLeft = 0
+        }
+    }, [children])
+
     return (
         <>
             <div className='w-full grid gap-6'>
                 <ComponentWrapper>
-                    <div>
-                        <h2 className='font-bold'>{title}</h2>
-                    </div>
+                    <Header
+                        title={title}
+                        nextHandler={nextOnClickHandler}
+                        previousHandler={previousOnClickHandler}
+                    />
                 </ComponentWrapper>
                 <div
+                    ref={CardCarousellContentRef}
                     className='w-full overflow-x-scroll'
                     style={{
                         scrollBehavior: 'smooth',
@@ -19,7 +58,12 @@ const CardCarousell = ({ title, children }) => {
                     }}
                 >
                     <ComponentWrapper>
-                        <div className='w-min flex gap-6'>{children}</div>
+                        <div
+                            ref={CardCarousellBoxRef}
+                            className='w-min flex gap-6'
+                        >
+                            {children}
+                        </div>
                     </ComponentWrapper>
                 </div>
             </div>
