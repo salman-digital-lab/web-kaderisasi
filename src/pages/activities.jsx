49bb9f3939ba5ff18fbeb7dcdbcad531763aed7b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 14:40:50 +0700

    fetch activityCategory

--- src/pages/activities.jsx
+++ src/pages/activities.jsx
@@ -4,10 +4,13 @@ import axios from 'axios'
 import { PageWrapper } from '@layout'
 import { ActivitiesModule } from '@modules'
 
-const activities = ({ activity }) => {
+const activities = ({ activity, activityCategory }) => {
     return (
         <PageWrapper title='Activities'>
-            <ActivitiesModule activityData={activity} />
+            <ActivitiesModule
+                activityData={activity}
+                activityCategory={activityCategory}
+            />
         </PageWrapper>
     )
 }
@@ -15,12 +18,16 @@ const activities = ({ activity }) => {
 const getStaticProps = async () => {
     const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
-    const response = await axios.get(`${baseURL}/v1/activity`)
+    const activitiesResponse = await axios.get(`${baseURL}/v1/activity`)
+    const activitiesCategoryResponse = await axios.get(
+        `${baseURL}/v1/activity-categories`
+    )
 
     return {
         props: {
             revalidate: 10,
-            activity: response.data.data.data,
+            activity: activitiesResponse.data.data.data,
+            activityCategory: activitiesCategoryResponse.data.data,
         },
     }
 }
