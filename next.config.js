62d1672026dffa89f049d7f53893608522d11aa5
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 17:33:53 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/login',
+                destination: '/auth/login',
+            },
+            {
+                source: '/register',
+                destination: '/auth/register',
+            },
+        ]
+    },
+
     reactStrictMode: true,
 }
+
+module.exports = nextConfig
