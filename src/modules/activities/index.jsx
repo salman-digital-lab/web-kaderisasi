5beedd87621c1df4a1118641824ac9e9863c0e99
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 13:53:38 +0700

    create activities module list

--- src/modules/activities/index.jsx
+++ src/modules/activities/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import { ComponentWrapper } from '@components'
+
+import ActivitiesModuleList from './list'
 import ActivitiesModuleHeader from './header'
 
-const ActivitiesModule = () => {
+const ActivitiesModule = ({ activityData }) => {
     return (
         <div>
             <ActivitiesModuleHeader />
+            <ComponentWrapper>
+                <ActivitiesModuleList activityData={activityData} />
+            </ComponentWrapper>
         </div>
     )
 }
---
+++ src/modules/activities/list/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActivitiesModuleList = () => {
+    return (
+        <div>
+            <h2>Daftar Kegiatan</h2>
+        </div>
+    )
+}
+
+export default ActivitiesModuleList
