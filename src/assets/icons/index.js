e0637e044d9d21ac61f0760645c5955851f11c90
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 14:13:24 +0700

    create search icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,7 @@
 import MailIcon from './mail'
 import LineIcon from './line'
 import PhoneIcon from './phone'
+import SearchIcon from './search'
 import BMKAFullIcon from './BMKAFull'
 import ArrowLeftIcon from './arrowLeft'
 import InstagramIcon from './instagram'
@@ -12,6 +13,7 @@ export {
     MailIcon,
     LineIcon,
     PhoneIcon,
+    SearchIcon,
     BMKAFullIcon,
     InstagramIcon,
     ArrowLeftIcon,
