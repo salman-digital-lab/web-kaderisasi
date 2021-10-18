fc5047ffd9a63daac4352e293e847fb9540a0251
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 21:09:37 +0700

    fix BMKA Auth Icon

--- src/assets/icons/BMKAAuth/index.jsx
+++ src/assets/icons/BMKAAuth/index.jsx
@@ -4,7 +4,10 @@ const BMKAAuthIcon = () => {
     return (
         <svg
             fill='none'
+            width='100%'
+            height='100%'
             viewBox='0 0 607 481'
+            preserveAspectRatio='none'
             xmlns='http://www.w3.org/2000/svg'
         >
             <path
