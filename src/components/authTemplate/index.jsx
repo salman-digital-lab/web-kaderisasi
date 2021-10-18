e783b1c417d89d5f1c0d0a53f4ac9f31e9007c3c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-18 21:43:57 +0700

    fix auth template

--- src/components/authTemplate/index.jsx
+++ src/components/authTemplate/index.jsx
@@ -8,8 +8,8 @@ const AuthTemplate = ({ children }) => {
             style={{ gridTemplateColumns: '1fr 1fr' }}
             className='fixed w-full h-full grid overflow-y-auto'
         >
-            <div className='w-full p-6 bg-white'>{children}</div>
-            <div className='w-full p-6 bg-bmka-primary-blue'>
+            <div className='w-full h-full p-6 bg-white'>{children}</div>
+            <div className='w-full h-full p-6 bg-bmka-primary-blue'>
                 <AuthTemplateSide />
             </div>
         </div>
