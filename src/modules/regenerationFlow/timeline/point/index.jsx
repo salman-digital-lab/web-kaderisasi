1a4db96c6a0e8bcbb8a28cc94e3632da38f5541a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:44:09 +0700

    create timeline point

---
+++ src/modules/regenerationFlow/timeline/point/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import RegenerationFlowModuleTimelineContent from '../content'
+
+const RegenerationFlowModuleTimelinePoint = ({
+    title,
+    number,
+    description,
+}) => {
+    return (
+        <div style={{ gridTemplateColumns: '1fr auto 1fr' }} className='grid'>
+            <div>
+                {number % 2 === 0 && (
+                    <RegenerationFlowModuleTimelineContent
+                        title={title}
+                        description={description}
+                    />
+                )}
+            </div>
+            <div className='flex items-center justify-center'>
+                <div className='w-12 h-12 flex items-center justify-center m-4 bg-blue-400 text-white font-bold rounded-full'>
+                    <p>{number <= 9 ? `0${number}` : number}</p>
+                </div>
+            </div>
+            <div>
+                {number % 2 !== 0 && (
+                    <RegenerationFlowModuleTimelineContent
+                        title={title}
+                        description={description}
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default RegenerationFlowModuleTimelinePoint
