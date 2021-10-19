91d9616819ee527e71f5c45fbe5b8a7ce03e20b9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 11:36:23 +0700

    passing some props

--- src/modules/activities/index.jsx
+++ src/modules/activities/index.jsx
@@ -7,20 +7,33 @@ import ActivitiesModuleHeader from './header'
 import ActivitiesModuleSearch from './search'
 
 const ActivitiesModule = ({ activityData, activityCategoryData }) => {
+    const [searchKeyword, setSearchKeyword] = useState('')
+    const [currentCategory, setCurrentCategory] = useState('')
     const [currentActivityData, setCurrentActivityData] = useState([
         ...activityData,
     ])
+
+    const clearFilterHandler = () => {
+        setSearchKeyword('')
+        setCurrentCategory('')
+        setCurrentActivityData(activityData)
+    }
 
     return (
         <div>
             <ActivitiesModuleHeader />
             <ComponentWrapper>
                 <ActivitiesModuleSearch
+                    searchKeyword={searchKeyword}
+                    currentCategory={currentCategory}
+                    setSearchKeyword={setSearchKeyword}
+                    setCurrentCategory={setCurrentCategory}
                     activityCategoryData={activityCategoryData}
                     setCurrentActivityData={setCurrentActivityData}
                 />
                 <ActivitiesModuleList
                     activityData={currentActivityData}
+                    clearFilterHandler={clearFilterHandler}
                     activityCategoryData={activityCategoryData}
                 />
             </ComponentWrapper>
