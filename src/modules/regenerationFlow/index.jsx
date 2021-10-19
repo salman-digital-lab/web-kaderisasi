9c0a59f7b37934ffcdf039e34fb9cd8ac7390eac
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:50:19 +0700

    implement alur kaderisasi timeline

--- src/modules/regenerationFlow/index.jsx
+++ src/modules/regenerationFlow/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+import { ComponentWrapper } from '@components'
+
 import RegenerationFlowModuleHeader from './header'
+import RegenerationFlowModuleTimeline from './timeline'
 
 const RegenerationFlowModule = () => {
     return (
         <div>
             <RegenerationFlowModuleHeader />
+            <ComponentWrapper>
+                <div className='py-16'>
+                    <div className='text-center'>
+                        <h2 className='font-bold text-bmka-primary-blue'>
+                            Bagaimana tahapannya ?
+                        </h2>
+                    </div>
+                    <RegenerationFlowModuleTimeline />
+                </div>
+            </ComponentWrapper>
         </div>
     )
 }
