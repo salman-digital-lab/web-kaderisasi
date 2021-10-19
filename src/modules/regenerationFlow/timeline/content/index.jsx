24a52c2398d6f7072d314b7639ef2046b26616e0
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:43:13 +0700

    create content

---
+++ src/modules/regenerationFlow/timeline/content/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RegenerationFlowModuleTimelineContent = ({ title, description }) => {
+    return (
+        <div>
+            <p className='text-2xl font-bold'>{title}</p>
+            <p className='pt-2 text-gray-600'>{description}</p>
+        </div>
+    )
+}
+
+export default RegenerationFlowModuleTimelineContent
