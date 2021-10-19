d5cdd9a552ef0252a471a33f5b27ac3bd00e66b0
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:49:15 +0700

    fix section

--- src/modules/regenerationFlow/timeline/section/index.jsx
+++ src/modules/regenerationFlow/timeline/section/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RegenerationFlowModuleTimelineSection = ({ children }) => {
     return (
-        <div className='w-28 h-28 flex flex-col items-center justify-center mx-auto bg-bmka-accent-orange rounded-full'>
+        <div className='w-28 h-28 m-4 flex flex-col items-center justify-center mx-auto bg-bmka-accent-orange rounded-full'>
             {children}
         </div>
     )
