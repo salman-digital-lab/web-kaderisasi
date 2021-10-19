7db6b818ba2c880e25ce79a2930b1fe92f1ed377
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:55:16 +0700

    update name

--- src/modules/regenerationFlow/header/index.jsx
+++ src/modules/regenerationFlow/header/index.jsx
@@ -8,7 +8,7 @@ const RegenerationFlowModuleHeader = () => {
         <Jumbotron>
             <div className='flex items-center justify-between'>
                 <div className='w-full max-w-lg flex flex-col gap-6'>
-                    <h1 className='text-white'>Jenjang Kaderisasi</h1>
+                    <h1 className='text-white'>Alur Kaderisasi</h1>
                     <p className='text-white'>
                         Lorem ipsum dolor sit, amet consectetur adipisicing
                         elit. Mollitia dignissimos necessitatibus, in nobis
