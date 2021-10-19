d13409cace77c77273f49997533f3f848d180a59
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 11:21:22 +0700

    split card grid

--- src/modules/activities/list/index.jsx
+++ src/modules/activities/list/index.jsx
@@ -1,56 +1,21 @@
-/* eslint-disable camelcase */
-/* eslint-disable react/no-array-index-key */
-
 import React from 'react'
 
 import { InfoIcon } from '@assets'
-import { formatDate } from '@utils'
-import { ActivityCard } from '@components'
+
+import ActivitiesModuleListCardGrid from './cardGrid'
 
 const ActivitiesModuleList = ({ activityData, activityCategoryData }) => {
     return (
         <div className='my-8'>
             {activityData.length > 0 ? (
-                <div
-                    className='w-full grid gap-4'
-                    style={{
-                        gridTemplateColumns:
-                            'repeat(auto-fill, minmax(270px, 1fr))',
-                    }}
-                >
-                    {activityData.map((item, index) => {
-                        const {
-                            name,
-                            role,
-                            slug,
-                            banner,
-                            created_at,
-                            category_id,
-                            register_end_date,
-                        } = item
-
-                        return (
-                            <div key={index} className='flex justify-center'>
-                                <ActivityCard
-                                    name={name}
-                                    role={role}
-                                    slug={slug}
-                                    banner={banner}
-                                    categoryId={category_id}
-                                    createdAt={formatDate(created_at)}
-                                    activityCategoryData={activityCategoryData}
-                                    registerEndDate={formatDate(
-                                        register_end_date
-                                    )}
-                                />
-                            </div>
-                        )
-                    })}
-                </div>
+                <ActivitiesModuleListCardGrid
+                    activityData={activityData}
+                    activityCategoryData={activityCategoryData}
+                />
             ) : (
                 <div className='w-full py-28'>
                     <div className='w-full max-w-sm mx-auto text-center'>
-                        <InfoIcon className='w-16 mx-auto' />
+                        <InfoIcon className='w-16 mx-auto text-bmka-accent-orange' />
                         <h3 className='mt-4'>Tidak ada data yang ditemukan</h3>
                     </div>
                 </div>
