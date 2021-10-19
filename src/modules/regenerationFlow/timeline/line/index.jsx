7b8ba5ecdaa10be845da84dc10de8b3c45799ca8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-19 14:49:35 +0700

    fix timeline line

---
+++ src/modules/regenerationFlow/timeline/line/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegenerationFlowModuleTimelineLine = () => {
+    return (
+        <div className='w-full'>
+            <div className='w-1 h-32 mx-auto bg-bmka-accent-orange' />
+        </div>
+    )
+}
+
+export default RegenerationFlowModuleTimelineLine
