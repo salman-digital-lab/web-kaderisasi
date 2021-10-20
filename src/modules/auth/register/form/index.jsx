8d1a270de03af0d2cbe2afc2e71fbd97f67fa2a8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-20 09:55:34 +0700

    fix register form

--- src/modules/auth/register/form/index.jsx
+++ src/modules/auth/register/form/index.jsx
@@ -3,28 +3,50 @@ import React from 'react'
 import { FormInput } from '@components'
 import { EmailIcon, UserIcon, LockIcon } from '@assets'
 
-const RegisterModuleForm = () => {
+const RegisterModuleForm = ({ formData, inputFormHandler }) => {
+    const { name, email, password, confirmPassword } = formData
+
     return (
         <div className='flex flex-col gap-4'>
             <FormInput
                 type='text'
+                name='name'
+                value={name}
+                autoComplete='off'
                 placeholder='Name'
+                onChange={inputFormHandler}
                 icon={<UserIcon className='w-6' />}
+                required
             />
             <FormInput
                 type='email'
+                name='email'
+                value={email}
+                autoComplete='off'
                 placeholder='Email'
+                onChange={inputFormHandler}
                 icon={<EmailIcon className='w-6' />}
+                required
             />
             <FormInput
                 type='password'
+                name='password'
+                value={password}
+                autoComplete='off'
                 placeholder='Password'
+                onChange={inputFormHandler}
                 icon={<LockIcon className='w-6' />}
+                required
             />
             <FormInput
                 type='password'
+                name='confirmPassword'
+                value={confirmPassword}
+                autoComplete='off'
                 placeholder='Confirm Password'
+                onChange={inputFormHandler}
                 icon={<LockIcon className='w-6' />}
+                required
             />
         </div>
     )
