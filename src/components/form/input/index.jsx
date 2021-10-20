8d9e34ec1e48064e265acffcc408c5fb13878275
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-20 09:25:44 +0700

    edit input

--- src/components/form/input/index.jsx
+++ src/components/form/input/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const FormInput = ({ icon, ...props }) => {
+const FormInput = ({ icon, className, ...props }) => {
     return (
         <div className='flex gap-2 items-center px-2 py-3 border-2 border-gray-500 rounded'>
             {icon && icon}
             <input
                 spellCheck='false'
-                className='w-full outline-none'
+                className={`w-full outline-none ${className ?? ''}`}
                 {...props}
             />
         </div>
