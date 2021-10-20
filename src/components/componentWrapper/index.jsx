c94e29562a04fad6e44afac2f880abd018500c71
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-20 08:04:56 +0700

    fix pageWrapper

--- src/components/componentWrapper/index.jsx
+++ src/components/componentWrapper/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ComponentWrapper = ({ children, className }) => {
     return (
-        <div className={`w-full px-6 max-w-7xl mx-auto ${className ?? ''}`}>
+        <div className={`w-full px-6 max-w-6xl mx-auto ${className ?? ''}`}>
             {children}
         </div>
     )
