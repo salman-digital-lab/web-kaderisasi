8bfcc559d55555485e577d8d96d7f17ea06b431c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-20 10:13:10 +0700

    fix login form

--- src/modules/auth/login/form/index.jsx
+++ src/modules/auth/login/form/index.jsx
@@ -3,18 +3,30 @@ import React from 'react'
 import { FormInput } from '@components'
 import { UserIcon, LockIcon } from '@assets'
 
-const LoginModuleForm = () => {
+const LoginModuleForm = ({ formData, inputFormHandler }) => {
+    const { email, password } = formData
+
     return (
         <div className='flex flex-col gap-4'>
             <FormInput
                 type='email'
+                name='email'
+                value={email}
+                autoComplete='off'
                 placeholder='Email'
+                onChange={inputFormHandler}
                 icon={<UserIcon className='w-6' />}
+                required
             />
             <FormInput
                 type='password'
+                name='password'
+                value={password}
+                autoComplete='off'
                 placeholder='Password'
+                onChange={inputFormHandler}
                 icon={<LockIcon className='w-6' />}
+                required
             />
         </div>
     )
