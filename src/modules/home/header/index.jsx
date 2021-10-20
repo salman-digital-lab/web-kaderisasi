04d5b6de96f387de6aa0b6c4fe0fc9ffb7ab723b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-20 14:00:31 +0700

    update home header

--- src/modules/home/header/index.jsx
+++ src/modules/home/header/index.jsx
@@ -10,9 +10,9 @@ const HomeModuleHeader = () => {
                 <div className='flex items-center'>
                     <div className='max-w-xl flex flex-col gap-6'>
                         <div className='flex flex-col gap-4'>
-                            <h1 className='text-white'>Selamat Datang</h1>
+                            <h1 className='text-white'>Selamat Datang di</h1>
                             <h1 className='text-white'>
-                                Di Kaderisasi Salman ITB
+                                Kaderisasi Salman ITB
                             </h1>
                         </div>
                         <p className='text-white'>
