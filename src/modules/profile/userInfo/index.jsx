678759d685db4e9ed43d2b5e6e4159ba39899ebd
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 20:22:18 +0700

    edit user profile

--- src/modules/profile/userInfo/index.jsx
+++ src/modules/profile/userInfo/index.jsx
@@ -15,7 +15,7 @@ const ProfileModuleUserInfo = () => {
                         className='w-28 h-28 object-cover rounded-full border-4 border-white transform translate-x-0 translate-y-1/2 bg-gray-300'
                     />
                 </div>
-                <div className='w-full pt-20 pb-4 px-4 flex flex-col items-center'>
+                <div className='w-full pt-12 pb-4 px-4 flex flex-col items-center'>
                     <div />
                     <div className='text-center pt-6 pb-10'>
                         <p className='text-xl font-bold'>
