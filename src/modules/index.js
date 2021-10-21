741f0beb5fe2c0b660abf790a4316d06a9ff9c44
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 15:47:55 +0700

    create profile module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import HomeModule from './home'
+import ProfileModule from './profile'
 import ActivitiesModule from './activities'
 import RegenerationFlowModule from './regenerationFlow'
 
-export { HomeModule, ActivitiesModule, RegenerationFlowModule }
+export { HomeModule, ProfileModule, ActivitiesModule, RegenerationFlowModule }
 export * from './auth'
---
+++ src/modules/profile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileModule = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default ProfileModule
