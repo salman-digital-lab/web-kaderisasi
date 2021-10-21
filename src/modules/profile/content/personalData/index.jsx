d7a628cee40ec9f766aeab9859ecb341b22fcbd3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 20:17:41 +0700

    adding ProfileModuleContentPersonalData

---
+++ src/modules/profile/content/personalData/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileModuleContentPersonalData = () => {
+    return (
+        <div>
+            <p>Personal Data</p>
+        </div>
+    )
+}
+
+export default ProfileModuleContentPersonalData
