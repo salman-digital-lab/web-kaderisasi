a896b58fb553669e0a9c0bdae5f2462708c61983
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 15:49:15 +0700

    remove unused code

--- src/modules/home/statistics/index.jsx
+++ src/modules/home/statistics/index.jsx
@@ -7,7 +7,6 @@ const HomeModuleStatistics = () => {
         <div
             className='w-full max-w-5xl mx-auto flex justify-between border-2 rounded-xl bg-white'
             style={{
-                // borderColor: '#8DBCDD',
                 transform: 'translate(0%,-50%)',
                 boxShadow: '0px 1px 15px rgba(0, 0, 0, 0.15)',
             }}
