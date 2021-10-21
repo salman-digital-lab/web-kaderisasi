85c54206218af62a4729a8548d851d97c7307bc2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 20:52:53 +0700

    create ProfileModuleContentPersonalDataTitle

---
+++ src/modules/profile/content/personalData/title/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ProfileModuleContentPersonalDataTitle = ({ children }) => {
+    return <p className='text-gray-400 font-bold text-lg pb-4'>{children}</p>
+}
+
+export default ProfileModuleContentPersonalDataTitle
