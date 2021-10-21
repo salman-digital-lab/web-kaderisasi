2b6713be2f47f52dc1ca64713060c5e2420f6b51
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-21 15:47:39 +0700

    create profile page

---
+++ src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { PageWrapper } from '@layout'
+import { ProfileModule } from '@modules'
+
+const profile = () => {
+    return (
+        <PageWrapper title='Profile'>
+            <ProfileModule />
+        </PageWrapper>
+    )
+}
+
+export default profile
