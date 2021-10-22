ecf2216039f515a904e98cf7135c78647cda356e
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-22 21:29:19 +0700

    fix zustand config

--- src/services/zustand/index.js
+++ src/services/zustand/index.js
@@ -2,14 +2,19 @@ import create from 'zustand'
 
 const setUser = (set, value) => set(() => ({ user: value }))
 
+const setIsAuthenticating = (set, value) =>
+    set(() => ({ isAuthenticating: value }))
+
 const setIsNavbarScrolling = (set, value) =>
     set(() => ({ isNavbarScrolling: value }))
 
 const zustandState = create((set) => ({
     user: null,
+    isAuthenticating: true,
     isNavbarScrolling: false,
 
     setUser: (value) => setUser(set, value),
+    setIsAuthenticating: (value) => setIsAuthenticating(set, value),
     setIsNavbarScrolling: (value) => setIsNavbarScrolling(set, value),
 }))
 
