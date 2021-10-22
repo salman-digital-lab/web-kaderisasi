33dbe46b987c84eeb44a92504b40a72debe82ff7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-22 20:49:29 +0700

    create authentication services

---
+++ src/services/authentication/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AuthenticationOverlay from './overlay'
+
+const Authentication = ({ children }) => {
+    return (
+        <div>
+            <AuthenticationOverlay />
+        </div>
+    )
+}
+
+export default Authentication
