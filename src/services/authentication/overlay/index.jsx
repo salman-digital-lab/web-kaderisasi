00e251e3cfc39c365f8e5ea3db7dcc63cbb5dcab
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-24 14:43:13 +0700

    remove overlay animation

--- src/services/authentication/overlay/index.jsx
+++ src/services/authentication/overlay/index.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 
 import { BMKAFullVerticalColorIcon } from '@assets'
 
 const AuthenticationOverlay = () => {
     return (
-        <div
-            style={{ zIndex: 999 }}
-            className='fixed w-full h-full flex flex-col items-center justify-center'
-        >
+        <div className='fixed w-full h-full flex flex-col items-center justify-center'>
             <div>
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    className='w-40 mx-auto'
-                >
+                <div className='w-40 mx-auto'>
                     <BMKAFullVerticalColorIcon />
-                </motion.div>
+                </div>
             </div>
         </div>
     )
