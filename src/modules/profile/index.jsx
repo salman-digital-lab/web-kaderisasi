4bf72ceba4215f7b26c00ccdc9cc18f7ac67e0c7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-24 13:52:07 +0700

    implement withUserAuthentication

--- src/modules/profile/index.jsx
+++ src/modules/profile/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { ComponentWrapper } from '@components'
+import { withUserAuthenticationHOC } from '@hoc'
 
 import ProfileModuleContent from './content'
 import ProfileModuleUserInfo from './userInfo'
@@ -18,4 +19,4 @@ const ProfileModule = ({ activityCategoryData }) => {
     )
 }
 
-export default ProfileModule
+export default withUserAuthenticationHOC(ProfileModule)
