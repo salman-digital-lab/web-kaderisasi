e3872d3939edde803f781da203b30b57fbe1809b
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-10-26 09:50:04 +0700

    hide image in sm breakpoint

--- src/components/authTemplate/index.jsx
+++ src/components/authTemplate/index.jsx
@@ -6,10 +6,10 @@ const AuthTemplate = ({ children }) => {
     return (
         <div
             style={{ gridTemplateColumns: '1fr 1fr' }}
-            className='fixed w-full h-full grid overflow-y-auto'
+            className='fixed w-full h-full md:grid overflow-y-auto'
         >
             <div className='w-full h-full p-6 bg-white'>{children}</div>
-            <div className='w-full h-full p-6 bg-bmka-primary-blue'>
+            <div className='hidden md:block w-full h-full p-6 bg-bmka-primary-blue'>
                 <AuthTemplateSide />
             </div>
         </div>
