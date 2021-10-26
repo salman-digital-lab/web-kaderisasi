24808ea409247f28c823608d4ca4be39dfb8c88e
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-10-26 11:08:03 +0700

    add responsive navbar

--- src/layout/navigation/index.jsx
+++ src/layout/navigation/index.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-shadow */
 
-import React from 'react'
+import React, { useState } from 'react'
 
-import { BMKAFullIcon } from '@assets'
+import { BMKAFullIcon, Menu } from '@assets'
 import { zustandStore } from '@services'
 import { ComponentWrapper } from '@components'
 
 import Route from './route'
 
 const Navigation = () => {
+    const [isActive, setIsActive] = useState(false)
+
     const state = {
         user: zustandStore((state) => state.user),
     }
@@ -32,17 +37,17 @@ const Navigation = () => {
     return (
         <nav className='fixed left-0 top-0 w-full py-4 bg-bmka-primary-blue z-50'>
             <ComponentWrapper>
-                <div className='flex justify-between'>
+                <div className='flex justify-between items-center'>
                     <div className='flex items-center w-32'>
                         <BMKAFullIcon />
                     </div>
-                    <div className='flex justify-center'>
+                    <div className='hidden md:flex justify-center'>
                         <Route href='/'>Home</Route>
                         <Route href='/activities'>Kegiatan</Route>
                         <Route href='/student-care'>Ruang Curhat</Route>
                         <Route href='/about-us'>Tentang Kami</Route>
                     </div>
-                    <div className='flex justify-center items-center'>
+                    <div className='hidden md:flex justify-center items-center'>
                         {!state.user ? (
                             <>
                                 <Route href='/register'>Daftar</Route>
@@ -53,6 +58,41 @@ const Navigation = () => {
                                 Hi, {formatName(state.user.name)}
                             </Route>
                         )}
+                    </div>
+                    <div
+                        role='button'
+                        className='md:hidden text-right'
+                        onClick={() => setIsActive(true)}
+                    >
+                        <Menu />
+                    </div>
+                    <div
+                        className={`fixed bg-bmka-primary-blue z-10  top-0 h-full w-full p-10 md:hidden transition-all ${
+                            isActive ? 'right-0' : '-right-full'
+                        }`}
+                    >
+                        <p
+                            className='transform rotate-45 text-white text-6xl absolute top-8 right-8 w-8'
+                            onClick={() => setIsActive(false)}
+                        >
+                            +
+                        </p>
+                        <div className=''>
+                            <Route href='/'>Home</Route>
+                            <Route href='/activities'>Kegiatan</Route>
+                            <Route href='/student-care'>Ruang Curhat</Route>
+                            <Route href='/about-us'>Tentang Kami</Route>
+                            {!state.user ? (
+                                <>
+                                    <Route href='/register'>Daftar</Route>
+                                    <Route href='/login'>Masuk</Route>
+                                </>
+                            ) : (
+                                <Route href='/profile'>
+                                    Hi, {formatName(state.user.name)}
+                                </Route>
+                            )}
+                        </div>
                     </div>
                 </div>
             </ComponentWrapper>
