a2038fca05f95c93b3986b319bf89fc517a1997a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-28 22:27:06 +0700

    create 404 page

---
+++ src/modules/404/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-underscore-dangle */
+
+import React from 'react'
+
+const _404 = () => {
+    return (
+        <div className='my-32'>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default _404
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,14 @@
+import _404 from './404'
 import HomeModule from './home'
 import ProfileModule from './profile'
 import ActivitiesModule from './activities'
 import RegenerationFlowModule from './regenerationFlow'
 
-export { HomeModule, ProfileModule, ActivitiesModule, RegenerationFlowModule }
+export {
+    _404,
+    HomeModule,
+    ProfileModule,
+    ActivitiesModule,
+    RegenerationFlowModule,
+}
 export * from './auth'
