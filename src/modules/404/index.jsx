f4fe1353af86790ba85b06f3bd6a0742d90cc5e5
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-28 22:42:07 +0700

    implement 404 content

--- src/modules/404/index.jsx
+++ src/modules/404/index.jsx
@@ -1,12 +1,37 @@
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable react/jsx-pascal-case */
 
 import React from 'react'
 
+import { Link, Button, ComponentWrapper } from '@components'
+
+import _404Icons from './404Icons'
+
 const _404Module = () => {
     return (
-        <div className='my-32'>
-            <p>Hello World</p>
-        </div>
+        <ComponentWrapper>
+            <div className='mb-32 mt-44 flex flex-col gap-6'>
+                <div className='w-32 mx-auto'>
+                    <_404Icons />
+                </div>
+                <div className='w-full max-w-lg mx-auto text-center'>
+                    <h2 className='font-bold text-bmka-primary-blue'>
+                        Oops! Halaman tidak ditemukan
+                    </h2>
+                    <p className='mt-4 text-gray-500'>
+                        Maaf, sayangnya kami tidak bisa menemukan halaman yang
+                        kamu cari. Silakan kembali ke portal utama ya.
+                    </p>
+                </div>
+                <div className='text-center'>
+                    <Link href='/'>
+                        <Button variant='primary'>
+                            Kembali ke portal utama
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </ComponentWrapper>
     )
 }
 
