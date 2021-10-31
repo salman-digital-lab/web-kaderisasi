939a8d325b9961d85d6541f4edbca6361d2ab563
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-31 10:17:53 +0700

    show some props

--- src/modules/activitiesDetail/banner/index.jsx
+++ src/modules/activitiesDetail/banner/index.jsx
@@ -2,30 +2,46 @@
 /* eslint-disable @next/next/no-img-element */
 
 import React from 'react'
-import Slider from 'react-slick'
 
-const ActivitiesDetailModuleBanner = ({ images }) => {
+import { formatDate, getActivityCategoryID } from '@utils'
+
+import ActivitiesDetailModuleBannerSlider from './slider'
+
+const ActivitiesDetailModuleBanner = ({
+    status,
+    images,
+    closeDate,
+    categoryID,
+    activityCategory,
+}) => {
     return (
-        <div className='w-full max-w-md'>
-            <Slider
-                dots={true}
-                speed={500}
-                infinite={true}
-                slidesToShow={1}
-                slidesToScroll={1}
-            >
-                {images.map((item) => {
-                    return (
-                        <div>
-                            <img
-                                src={item}
-                                alt='Banner'
-                                className='w-full max-w-md'
-                            />
-                        </div>
-                    )
-                })}
-            </Slider>
+        <div className='w-full max-w-lg mx-auto'>
+            <div className='w-full'>
+                {/* <ActivitiesDetailModuleBannerSlider images={images} /> */}
+            </div>
+            <div className='w-full flex justify-between'>
+                <div>
+                    <p className='text-gray-500 text-sm'>Kategori</p>
+                    <p className='text-lg font-bold text-bmka-primary-blue'>
+                        {getActivityCategoryID({
+                            activityCategoryID: categoryID,
+                            activityCategoryData: activityCategory,
+                        })}
+                    </p>
+                </div>
+                <div>
+                    <p className='text-gray-500 text-sm'>Status</p>
+                    <p className='text-lg font-bold text-bmka-primary-blue'>
+                        {status}
+                    </p>
+                </div>
+                <div>
+                    <p className='text-gray-500 text-sm'>Tutup Pendaftaran</p>
+                    <p className='text-lg font-bold text-bmka-primary-blue'>
+                        {formatDate(closeDate)}
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
