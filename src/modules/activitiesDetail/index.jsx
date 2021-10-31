c70337934d43ed12d24d3a3a9d562fc2711b6009
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-31 18:58:41 +0700

    implement fallback component

--- src/modules/activitiesDetail/index.jsx
+++ src/modules/activitiesDetail/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
-import { Jumbotron, ComponentWrapper } from '@components'
+import { Fallback, Jumbotron, ComponentWrapper } from '@components'
 
 import ActivitiesDetailModuleList from './list'
 import ActivitiesDetailModuleBanner from './banner'
@@ -11,37 +12,47 @@ const ActivitiesDetailModule = ({
     activityCategory,
     activityDetailData,
 }) => {
+    const router = useRouter()
+
     return (
         <div>
-            <Jumbotron>
-                <h1 className='text-center text-white'>
-                    {activityDetailData.name}
-                </h1>
-            </Jumbotron>
-            <ComponentWrapper>
+            {router.isFallback ? (
+                <Fallback />
+            ) : (
                 <div>
-                    <div>
-                        <ActivitiesDetailModuleBanner
-                            status={activityDetailData.status}
-                            images={activityDetailData.images}
-                            activityCategory={activityCategory}
-                            categoryID={activityDetailData.category_id}
-                            closeDate={activityDetailData.register_end_date}
-                        />
-                    </div>
-                    <div>
-                        <ActivitiesDetailModuleContent
-                            description={activityDetailData.description}
+                    <Jumbotron>
+                        <h1 className='text-center text-white'>
+                            {activityDetailData.name}
+                        </h1>
+                    </Jumbotron>
+                    <ComponentWrapper>
+                        <div>
+                            <div>
+                                <ActivitiesDetailModuleBanner
+                                    status={activityDetailData.status}
+                                    images={activityDetailData.images}
+                                    activityCategory={activityCategory}
+                                    categoryID={activityDetailData.category_id}
+                                    closeDate={
+                                        activityDetailData.register_end_date
+                                    }
+                                />
+                            </div>
+                            <div>
+                                <ActivitiesDetailModuleContent
+                                    description={activityDetailData.description}
+                                />
+                            </div>
+                        </div>
+                    </ComponentWrapper>
+                    <div className='py-20'>
+                        <ActivitiesDetailModuleList
+                            activityData={activityList}
+                            activityCategoryData={activityCategory}
                         />
                     </div>
                 </div>
-            </ComponentWrapper>
-            <div className='py-20'>
-                <ActivitiesDetailModuleList
-                    activityData={activityList}
-                    activityCategoryData={activityCategory}
-                />
-            </div>
+            )}
         </div>
     )
 }
