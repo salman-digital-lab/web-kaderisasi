c58680a202d4255860ade903af0a793bf85aaa41
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-11-01 04:26:04 +0700

    implement student care form

--- src/modules/studentCare/index.jsx
+++ src/modules/studentCare/index.jsx
@@ -1,9 +1,39 @@
+/* eslint-disable no-shadow */
+
 import React from 'react'
 
+import { zustandStore } from '@services'
+import { Jumbotron, ComponentWrapper } from '@components'
+
+import StudentCareModuleForm from './form'
+
 const StudentCareModule = () => {
+    const state = {
+        user: zustandStore((state) => state.user),
+    }
+
     return (
         <div>
-            <p>Hello Worlds</p>
+            <Jumbotron>
+                <div className='w-full max-w-2xl mx-auto text-center flex flex-col gap-6'>
+                    <h1 className='text-white'>Ruang Curhat</h1>
+                    <p className='text-white'>
+                        Ruang Curhat adalah layanan pengajuan dan pendampingan
+                        masalah untuk aktivis Salman ITB. Kamu akan ditemani
+                        langsung oleh tim Kaderisasi Salman yang berlatar
+                        belakang Psikologi dan Psikoterapi.
+                    </p>
+                </div>
+            </Jumbotron>
+            <ComponentWrapper>
+                <div className='w-full max-w-xl mb-10 p-6 mx-auto transform -translate-y-10 rounded bg-white shadow-level-1'>
+                    {state.user ? (
+                        <StudentCareModuleForm name={state.user.name} />
+                    ) : (
+                        <p>Kamu harus login dulu</p>
+                    )}
+                </div>
+            </ComponentWrapper>
         </div>
     )
 }
