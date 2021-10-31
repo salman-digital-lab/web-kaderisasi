328a8425ad2c164a28ff700c4b70202516778ac2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-10-31 21:09:55 +0700

    create student care module

---
+++ src/modules/studentCare/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentCareModule = () => {
+    return (
+        <div>
+            <p>Hello Worlds</p>
+        </div>
+    )
+}
+
+export default StudentCareModule
