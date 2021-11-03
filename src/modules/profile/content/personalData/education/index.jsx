5bc21a13c63c9438bd2910f08cd50f7ab26eb0e1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-11-03 14:11:25 +0700

    implement state to education section

--- src/modules/profile/content/personalData/education/index.jsx
+++ src/modules/profile/content/personalData/education/index.jsx
@@ -4,16 +4,54 @@ import { FormInput, FormSelect } from '@components'
 
 import ProfileModuleContentPersonalDataCard from '../card'
 
-const ProfileModuleContentPersonalDataEducation = () => {
+const ProfileModuleContentPersonalDataEducation = ({
+    formData,
+    formOnChangeHandler,
+}) => {
     return (
         <ProfileModuleContentPersonalDataCard>
-            <FormSelect value='' label='Kampus' placeholder='Pilih Kampus'>
+            <FormInput
+                type='text'
+                name='student_id'
+                value={formData.student_id}
+                label='Nomor Induk Mahasiswa'
+                onChange={formOnChangeHandler}
+                placeholder='-- Ketikkan nomor induk mahasiswa --'
+            />
+            <FormSelect
+                label='Kampus'
+                value={formData.university_id}
+                onChange={formOnChangeHandler}
+                placeholder='-- Pilih kampus anda --'
+            >
                 <option value='UNIKOM'>UNIKOM</option>
                 <option value='UNIKOM'>ITB</option>
                 <option value='UNIKOM'>UNPAD</option>
             </FormSelect>
-            <FormInput type='text' label='Fakultas' />
-            <FormInput type='text' label='Program Studi' />
+            <FormInput
+                type='text'
+                name='faculty'
+                label='Fakultas'
+                value={formData.faculty}
+                onChange={formOnChangeHandler}
+                placeholder='-- Ketikkan fakultas anda --'
+            />
+            <FormInput
+                type='text'
+                name='major'
+                label='Program Studi'
+                value={formData.major}
+                onChange={formOnChangeHandler}
+                placeholder='-- Ketikkan program studi anda --'
+            />
+            <FormInput
+                type='number'
+                name='intake_year'
+                label='Tahun Masuk'
+                value={formData.intake_year}
+                onChange={formOnChangeHandler}
+                placeholder='-- Ketikkan tahun masuk anda --'
+            />
         </ProfileModuleContentPersonalDataCard>
     )
 }
