1f7c07977d047152c4718abe0d117082ca0a77b3
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-11-03 10:01:22 +0700

    create activity register module

---
+++ src/modules/activitiesRegister/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ActivitesRegister = (props) => {
+    console.log(props)
+
+    return (
+        <>
+            <h1>activitiesRegister</h1>
+        </>
+    )
+}
+
+export default ActivitesRegister
