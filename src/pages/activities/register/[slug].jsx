5973730c8a89f14922f8b0011674c1931185b23d
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-11-03 13:18:02 +0700

    Update [slug].jsx

--- src/pages/activities/register/[slug].jsx
+++ src/pages/activities/register/[slug].jsx
@@ -1,22 +1,49 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-shadow */
 import React from 'react'
 import { PageWrapper } from '@layout'
-import ActivitesRegister from '../../../modules/activitiesRegister'
+import { ActivitesRegisterModule } from '@modules'
+import axios from 'axios'
 
-const activitiesRegister = ({ slug }) => {
+const activitiesRegister = ({ form }) => {
     return (
         <>
             <PageWrapper title='Pendaftaran Kegiatan'>
-                <ActivitesRegister slug={slug} />
+                <ActivitesRegisterModule
+                    status={form.status}
+                    message={form.message}
+                />
             </PageWrapper>
         </>
     )
 }
 
+export default activitiesRegister
+
 export async function getServerSideProps(ctx) {
+    const { req } = ctx
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+    const { cookies } = req
     const { slug } = ctx.params
+
+    const questionnaire = await axios
+        .get(`${baseURL}/v1/activity/register/${slug}`, {
+            headers: {
+                Authorization: `Bearer ${cookies.user}`,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+        .then((res) => {
+            return res
+        })
+        .catch((error) => {
+            return error.response.data
+        })
+
+    const form = JSON.parse(JSON.stringify(questionnaire))
+
     return {
-        props: { slug }, // will be passed to the page component as props
+        props: { form }, // will be passed to the page component as props
     }
 }
-
-export default activitiesRegister
