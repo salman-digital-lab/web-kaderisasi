1c8d01f664e079e293ca33ff083ccc9987a55819
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-11-05 20:02:04 +0700

    Create form radio

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,5 +2,6 @@ import FormInput from './input'
 import FormSelect from './select'
 import AuthInput from './authInput'
 import FormTextArea from './textArea'
+import FormRadio from './radio'
 
-export { FormInput, FormSelect, AuthInput, FormTextArea }
+export { FormInput, FormSelect, AuthInput, FormTextArea, FormRadio }
