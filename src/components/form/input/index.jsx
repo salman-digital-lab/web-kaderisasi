9fb49a6651939342b7ff90e96e017ce09019d829
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-11-05 20:02:23 +0700

    add * if required

--- src/components/form/input/index.jsx
+++ src/components/form/input/index.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const FormInput = ({ label, className, ...props }) => {
     return (
         <div>
-            {label && <p>{label}</p>}
+            {label && (
+                <p>
+                    {label}
+                    {props.required === true && (
+                        <span style={{ color: 'red' }}> *</span>
+                    )}
+                </p>
+            )}
             <div className='flex gap-2 items-center px-2 py-3 border-2 border-bmka-primary-blue rounded'>
                 <input
                     spellCheck='false'
