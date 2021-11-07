68bdddde0651a9a3648ce577e4bcf5683e09f7d0
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-11-07 19:01:33 +0700

    fix style jumbotron

--- src/components/jumbotron/index.jsx
+++ src/components/jumbotron/index.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 
 import { ComponentWrapper } from '@components'
 
-const Jumbotron = ({ children, className }) => {
+const Jumbotron = ({ children }) => {
     return (
-        <div
-            className={`w-full bg-bmka-primary-blue pt-40 pb-10 ${
-                className ?? ''
-            }`}
-        >
+        <div className='w-full bg-bmka-primary-blue pt-40 pb-10'>
             <ComponentWrapper>{children}</ComponentWrapper>
         </div>
     )
