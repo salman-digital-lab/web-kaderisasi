76bc00411bd32b6d8aa31789f6055b428e0c213d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-11-08 12:17:14 +0700

    passing regency, district and village data

--- src/modules/profile/content/index.jsx
+++ src/modules/profile/content/index.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import axios from 'axios'
+import { useSnackbar } from 'notistack'
 import React, { useState, useEffect } from 'react'
 
 import { zustandStore } from '@services'
@@ -25,6 +26,8 @@ const ProfileModuleContent = ({
         activities: 'activities',
     }
 
+    const { enqueueSnackbar } = useSnackbar()
+
     const state = {
         user: zustandStore((state) => state.user),
         setUser: zustandStore((state) => state.setUser),
@@ -34,19 +37,60 @@ const ProfileModuleContent = ({
     const [formData, setFormData] = useState({ ...state.user })
     const [regionData, setRegionData] = useState({
         regency: [],
+        village: [],
+        district: [],
     })
 
+    // get regencies data
     useEffect(async () => {
         if (!formData.province_id) {
             return
         }
 
-        const response = await axios.get(
-            `${baseURL}/${baseURLVersion}/regions/regencies/${formData.province_id}`
-        )
+        try {
+            const response = await axios.get(
+                `${baseURL}/${baseURLVersion}/regions/regencies/${formData.province_id}`
+            )
 
-        setRegionData({ ...regionData, regency: response.data.data })
+            setRegionData({ ...regionData, regency: response.data.data })
+        } catch {
+            enqueueSnackbar('Failed to get region data', { variant: 'error' })
+        }
     }, [formData.province_id])
+
+    // get district data
+    useEffect(async () => {
+        if (!formData.regency_id) {
+            return
+        }
+
+        try {
+            const response = await axios.get(
+                `${baseURL}/${baseURLVersion}/regions/districts/${formData.regency_id}`
+            )
+
+            setRegionData({ ...regionData, district: response.data.data })
+        } catch {
+            enqueueSnackbar('Failed to get region data', { variant: 'error' })
+        }
+    }, [formData.regency_id])
+
+    // get village data
+    useEffect(async () => {
+        if (!formData.district_id) {
+            return
+        }
+
+        try {
+            const response = await axios.get(
+                `${baseURL}/${baseURLVersion}/regions/villages/${formData.district_id}`
+            )
+
+            setRegionData({ ...regionData, village: response.data.data })
+        } catch {
+            enqueueSnackbar('Failed to get region data', { variant: 'error' })
+        }
+    }, [formData.district_id])
 
     return (
         <div className='w-full'>
