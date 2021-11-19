03ed93854b6d1aed13a99fb2f7fdb7478e4834ed
Farhan Rafly Fahrezi S <raflyfahr15@gmail.com>
2021-11-19 12:31:21 +0700

    fix margin header activities detail

--- src/modules/activitiesDetail/index.jsx
+++ src/modules/activitiesDetail/index.jsx
@@ -21,7 +21,7 @@ const ActivitiesDetailModule = ({
             ) : (
                 <div>
                     <Jumbotron>
-                        <h1 className='w-5/6 mx-auto md:w-full text-center text-white px-1 md:px-5 text-3xl md:text-6xl'>
+                        <h1 className='mb-16 w-5/6 mx-auto md:w-full text-center text-white px-1 md:px-5 text-3xl md:text-6xl'>
                             {activityDetailData.name}
                         </h1>
                     </Jumbotron>
