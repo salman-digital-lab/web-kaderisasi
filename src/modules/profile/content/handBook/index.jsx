d9d3596ac910bce02d44884f31a1c36437ff16df
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-02 12:41:38 +0700

    implement handbook checkbox

--- src/modules/profile/content/handBook/index.jsx
+++ src/modules/profile/content/handBook/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const ProfileModuleContentHandBook = () => {
+import HandbookCheckbox from './checkbox'
+
+const ProfileModuleContentHandBook = ({
+    listChecklist,
+    userChecklist,
+    setUserChecklist,
+}) => {
     return (
         <div>
-            <p>Hand Book</p>
+            <div
+                className='mb-10 rounded'
+                style={{ backgroundColor: '#F4FAFC' }}
+            >
+                <div className='px-6 py-4 rounded bg-bmka-primary-blue'>
+                    <h3 className='font-bold text-white'>Daftar Misi</h3>
+                </div>
+                <div className='p-4 flex flex-col gap-4'>
+                    {listChecklist.map((item) => {
+                        return (
+                            <HandbookCheckbox
+                                id={item.id}
+                                name={item.checklist_name}
+                            />
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
