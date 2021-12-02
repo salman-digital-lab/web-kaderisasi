f7d9d954a18709a7cc025bda5ebb2143d54feee8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-02 14:06:32 +0700

    styling HandbookCheckbox

--- src/modules/profile/content/handBook/checkbox/index.jsx
+++ src/modules/profile/content/handBook/checkbox/index.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useRef, useState } from 'react'
 
-const HandbookCheckbox = ({ id, name }) => {
+const HandbookCheckbox = ({ id, name, value }) => {
+    const checkboxRef = useRef()
+    const [checkboxValue, setCheckboxValue] = useState(value)
+
+    const checkboxOnClick = () => {
+        setCheckboxValue(!checkboxValue)
+    }
+
     return (
-        <div
+        <motion.button
             key={id}
-            className='p-4 flex gap-6 items-center border-2 rounded bg-white cursor-pointer'
+            type='button'
+            onClick={checkboxOnClick}
+            whileTap={{ scale: 0.98 }}
+            className={`p-4 flex gap-6 items-center border-2 rounded cursor-pointer ${
+                checkboxValue ? 'bg-gray-200' : 'bg-white'
+            }`}
         >
             <input
                 type='checkbox'
+                ref={checkboxRef}
                 id={`task-${id}`}
                 name={`task-${id}`}
+                checked={checkboxValue}
+                onClick={checkboxOnClick}
                 className='cursor-pointer'
             />
-            <label className='cursor-pointer' htmlFor={`task-${id}`}>
-                {name}
-            </label>
-        </div>
+            <p className={`${checkboxValue ? 'line-through' : ''}`}>{name}</p>
+        </motion.button>
     )
 }
 
