4355749ac7d2261790d6b5bf7c7d3a55ff44d512
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2021-12-10 10:13:14 +0700

    create utils to sanitize html

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import formatDate from './formatDate'
 import getUserRoleID from './getUserRoleID'
 import getActivityCategoryID from './getActivityCategoryID'
+import sanitize from './sanitize'
 
-export { formatDate, getUserRoleID, getActivityCategoryID }
+export { formatDate, getUserRoleID, getActivityCategoryID, sanitize }
