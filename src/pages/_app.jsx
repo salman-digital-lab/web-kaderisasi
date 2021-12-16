266f5ad62ba4f4be5efeebee0980d3602eca4efb
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-16 09:16:12 +0700

    import typeface-roboto

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NextProgress from 'nextjs-progressbar'
 import { SnackbarProvider } from 'notistack'
 
+import 'typeface-roboto'
 import 'tailwindcss/tailwind.css'
 
 import '@styles/global.css'
