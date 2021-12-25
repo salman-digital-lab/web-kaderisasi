b2d907b54bc7dc8639f06d021b484737b1b2631f
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-25 19:34:46 +0700

    fix footer title

--- src/layout/footer/title/index.jsx
+++ src/layout/footer/title/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterTitle = ({ children }) => {
     return (
-        <div className='flex pb-6'>
+        <div className='flex pb-6 justify-center sm:justify-start'>
             <p className='pb-2 text-white font-bold border-b-2 border-bmka-accent-orange'>
                 {children}
             </p>
