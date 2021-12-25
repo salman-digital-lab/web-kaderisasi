a09f42ccdcaf5cef28aa3a35902c4d645bdcc6e5
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-25 19:07:17 +0700

    fix home statistics shadow

--- src/modules/home/statistics/index.jsx
+++ src/modules/home/statistics/index.jsx
@@ -4,13 +4,7 @@ import Box from './box'
 
 const HomeModuleStatistics = () => {
     return (
-        <div
-            className='w-full max-w-5xl mx-auto flex flex-col justify-between border-2 rounded-xl bg-white transform -translate-y-16 md:flex-row md:-translate-y-1/2'
-            style={{
-                // transform: 'translate(0%,-50%)',
-                boxShadow: '0px 1px 15px rgba(0, 0, 0, 0.15)',
-            }}
-        >
+        <div className='w-full max-w-5xl mx-auto flex flex-col justify-between border-2 rounded-xl bg-white transform -translate-y-16 shadow-level-1 md:flex-row md:-translate-y-1/2'>
             <Box number='50' title='Kampus di seluruh Indonesia' />
             <Box number='1200' title='Kader dari kalangan mahasiswa' />
             <Box number='20' title='Provinsi di seluruh Indonesia' />
