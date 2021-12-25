dc22aec55c3e41d67e2bff02e8c0f3c05eb520bf
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-25 18:58:13 +0700

    fix statistics

--- src/modules/home/statistics/index.jsx
+++ src/modules/home/statistics/index.jsx
@@ -5,9 +5,9 @@ import Box from './box'
 const HomeModuleStatistics = () => {
     return (
         <div
-            className='w-full max-w-5xl mx-auto flex justify-between border-2 rounded-xl bg-white'
+            className='w-full max-w-5xl mx-auto flex flex-col justify-between border-2 rounded-xl bg-white transform -translate-y-16 md:flex-row md:-translate-y-1/2'
             style={{
-                transform: 'translate(0%,-50%)',
+                // transform: 'translate(0%,-50%)',
                 boxShadow: '0px 1px 15px rgba(0, 0, 0, 0.15)',
             }}
         >
