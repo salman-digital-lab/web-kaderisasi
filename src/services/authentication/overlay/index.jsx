58bb58190486fa0a8fff9c597a922c2f7f41b6e4
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-01-05 14:40:05 +0700

    adding some id

--- src/services/authentication/overlay/index.jsx
+++ src/services/authentication/overlay/index.jsx
@@ -4,7 +4,10 @@ import { BMKAFullVerticalColorIcon } from '@assets'
 
 const AuthenticationOverlay = () => {
     return (
-        <div className='fixed w-full h-full flex flex-col items-center justify-center bg-white'>
+        <div
+            id='overlay'
+            className='fixed w-full h-full flex flex-col items-center justify-center bg-white'
+        >
             <div>
                 <div className='w-40 mx-auto'>
                     <BMKAFullVerticalColorIcon />
