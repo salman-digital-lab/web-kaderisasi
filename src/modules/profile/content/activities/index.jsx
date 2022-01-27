dd142850f287c0c4aa17758aa448de2a01c25ca6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-01-27 14:39:51 +0700

    show activity card

--- src/modules/profile/content/activities/index.jsx
+++ src/modules/profile/content/activities/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-import { Button, FormInput, FormSelect } from '@components'
+import { formatDate } from '@utils'
+import { ActivityCard, Button, FormInput, FormSelect } from '@components'
 
 const ProfileModuleContentActivities = ({
     userActivities,
     activityCategoryData,
 }) => {
+    console.log(userActivities)
+
     return (
         <div>
             <div className='flex flex-col-reverse gap-6 justify-between items-center sm:flex-row'>
@@ -39,14 +42,49 @@ const ProfileModuleContentActivities = ({
                     <Button variant='secondary'>Cari</Button>
                 </form>
             </div>
-            <div>
-                {/* {userActivities.length > 0 ? (
-                    <div />
+            <div
+                className='w-full grid gap-6 pt-6'
+                style={{
+                    gridTemplateColumns:
+                        'repeat(auto-fill, minmax(270px, 1fr))',
+                }}
+            >
+                {userActivities?.length > 0 ? (
+                    <div>
+                        {userActivities?.map((item) => {
+                            const {
+                                name,
+                                minimum_role,
+                                slug,
+                                created_at,
+                                category_id,
+                                banner_image,
+                                register_end_date,
+                            } = item
+
+                            return (
+                                <ActivityCard
+                                    key={slug}
+                                    name={name}
+                                    slug={slug}
+                                    role={minimum_role?.name}
+                                    categoryId={category_id}
+                                    banner={banner_image?.file}
+                                    createdAt={formatDate(created_at)}
+                                    activityCategoryData={activityCategoryData}
+                                    registerEndDate={formatDate(
+                                        register_end_date
+                                    )}
+                                    fullWidth
+                                />
+                            )
+                        })}
+                    </div>
                 ) : (
                     <div className='w-full py-20 text-center'>
                         <p>Tidak ada kegiatan</p>
                     </div>
-                )} */}
+                )}
             </div>
         </div>
     )
