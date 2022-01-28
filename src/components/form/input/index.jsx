287a43cd080637223dcc91aa3e1da3d7980384ef
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-01-28 07:15:01 +0700

    add required props

--- src/components/form/input/index.jsx
+++ src/components/form/input/index.jsx
@@ -17,6 +17,7 @@ const FormInput = ({ label, className, required, fullWidth, ...props }) => {
                 } flex gap-2 items-center px-2 py-3 border-2 border-bmka-primary-blue rounded`}
             >
                 <input
+                    required={required}
                     spellCheck='false'
                     className={`w-full outline-none text-base ${
                         className ?? ''
--- src/components/form/textArea/index.jsx
+++ src/components/form/textArea/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const FormTextArea = ({ label, className, ...props }) => {
+const FormTextArea = ({ label, required, className, ...props }) => {
     return (
         <div>
-            {label && <p>{label}</p>}
+            {label && (
+                <p>
+                    {label}
+                    {required === true && (
+                        <span style={{ color: 'red' }}> *</span>
+                    )}
+                </p>
+            )}
             <div className='flex gap-2 items-center px-2 py-3 border-2 border-bmka-primary-blue rounded'>
                 <textarea
+                    required={required}
                     className={`w-full outline-none resize-none ${
                         className ?? ''
                     }`}
