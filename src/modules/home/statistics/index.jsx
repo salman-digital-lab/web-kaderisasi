6223fb3e5c97a8cd2b9ee19d6749d209ffaf6261
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-08 09:21:38 +0700

    pass statistic props

--- src/modules/home/statistics/index.jsx
+++ src/modules/home/statistics/index.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 import Box from './box'
 
-const HomeModuleStatistics = () => {
+const HomeModuleStatistics = ({ university, kader, province }) => {
     return (
         <div className='w-full max-w-5xl mx-auto flex flex-col justify-between border-2 rounded-xl bg-white transform -translate-y-16 shadow-level-1 md:flex-row md:-translate-y-1/2'>
-            <Box number='50' title='Kampus di seluruh Indonesia' />
-            <Box number='1200' title='Kader dari kalangan mahasiswa' />
-            <Box number='20' title='Provinsi di seluruh Indonesia' />
+            <Box number={university} title='Kampus di seluruh Indonesia' />
+            <Box number={kader} title='Kader dari kalangan mahasiswa' />
+            <Box number={province} title='Provinsi di seluruh Indonesia' />
         </div>
     )
 }
