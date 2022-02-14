f42dbe8b11ff464755eb35a2072c4e41487b9f62
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-02-14 21:41:54 +0700

    add tawk.to to page wrapper

--- src/layout/pageWrapper/index.jsx
+++ src/layout/pageWrapper/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Authentication } from '@services'
 import { Head, Footer, Navigation } from '@layout'
+import TawkTo from 'tawkto-react'
 
 const PageWrapper = ({ title, children, plain }) => {
+    useEffect(() => {
+        var tawk = new TawkTo('5e36a790a89cda5a1883af2f', 'default')
+    }, [])
     return (
         <>
             <Head title={title} />
