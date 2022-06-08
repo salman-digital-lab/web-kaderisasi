ead6dfc28e8d9d0b9a39c0b715455d144a89b615
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-06-09 06:53:26 +0700

    fix scrolling

--- src/components/form/dropdown/index.jsx
+++ src/components/form/dropdown/index.jsx
@@ -19,17 +19,22 @@ const FormDropdown = ({
     const itemRef = useRef(null)
     const [data, setData] = useState([])
     const [isOpen, setIsOpen] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
 
     const handleInputType = (input) => {
         let filteredData = options.filter((item) =>
             item.name.toLowerCase().includes(input.target.value)
         )
-        setData(filteredData)
+        if (filteredData.length === 0) {
+            setIsEmpty(true)
+        } else {
+            setIsEmpty(false)
+        }
         setIsOpen(true)
+        setData(filteredData)
     }
 
     const handleClick = (e, selected) => {
-        console.log(e)
         inputRef.current.value = selected.name
         onChange(e)
     }
@@ -65,32 +70,41 @@ const FormDropdown = ({
                     defaultValue={defaultName || ''}
                     placeholder={placeholder}
                     onChange={handleInputType}
-                    // onClick={() => (inputRef.current.value = '')}
+                    onClick={() => (inputRef.current.value = '')}
                     onBlur={handleBlur}
                     {...props}
                 />
                 {isOpen ? (
-                    <ul
-                        className='absolute max-w-sm mt-10 ml-15 bg-white border-2 border-bmka-primary-blue rounded group-focus:bg-red-300'
-                        // onClick={() => setIsOpen(false)}
-                    >
-                        {data.length > 0
-                            ? data.map((item) => {
-                                  return (
-                                      <button
-                                          ref={itemRef}
-                                          name={name}
-                                          key={item.id}
-                                          className='flex flex-col p-1 border-b-2 text-left outline-none text-sm bg-white hover:bg-blue-100 '
-                                          type='button'
-                                          onClick={(e) => handleClick(e, item)}
-                                          value={item.id}
-                                      >
-                                          {item.name}
-                                      </button>
-                                  )
-                              })
-                            : null}
+                    <ul className='absolute w-60 max-h-80 overflow-y-auto mt-10 ml-15 bg-white border-2 border-bmka-primary-blue rounded group-focus:bg-red-300'>
+                        {!isEmpty ? (
+                            data.map((item) => {
+                                return (
+                                    <li className='z-50 border-b-2 hover:bg-bmka-primary-blue hover:text-white'>
+                                        <button
+                                            ref={itemRef}
+                                            name={name}
+                                            key={item.id}
+                                            className='flex flex-col p-1 text-left outline-none text-sm  '
+                                            type='button'
+                                            onClick={(e) =>
+                                                handleClick(e, item)
+                                            }
+                                            value={item.id}
+                                        >
+                                            {item.name}
+                                        </button>
+                                    </li>
+                                )
+                            })
+                        ) : (
+                            <button
+                                className='flex flex-col overflow-auto p-1 border-b-2 text-left outline-none text-sm text-red-600'
+                                type='button'
+                                disabled
+                            >
+                                No results
+                            </button>
+                        )}
                     </ul>
                 ) : null}
             </div>
