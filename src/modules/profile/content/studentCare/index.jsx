30b693f9cfcfcc3f00ead65f8099ddd2ce1b17e5
naufalzhafran <latifnaufal7@gmail.com>
2023-02-20 10:23:39 +0700

    feat: map studentCare data

--- src/modules/profile/content/studentCare/index.jsx
+++ src/modules/profile/content/studentCare/index.jsx
@@ -23,10 +23,10 @@ const ProfileModuleStudentCare = () => {
                 }
             )
 
-            return await response.data.data
+            return await response.data.data.data
         }
         try {
-            const studentCareFetchData = getStudentCareData()
+            const studentCareFetchData = await getStudentCareData()
             setStudentCareData(studentCareFetchData)
         } catch (error) {
             enqueueSnackbar('Error getting region data', { variant: 'error' })
@@ -34,56 +34,66 @@ const ProfileModuleStudentCare = () => {
     }, [])
 
     return (
-        <div>
-            <div className='p-4 flex flex-col justify-between border-2 bg-white cursor-default border-bmka-primary-blue rounded'>
-                <p className='text-sm text-gray-400'>ID : 34f34ed2r443</p>
-                <p className='text-sm text-gray-400'>13/2/2022</p>
+        <div className='flex flex-col gap-3'>
+            {studentCareData
+                ? studentCareData.map((item) => (
+                      <div
+                          key={item.id}
+                          className='p-4 flex flex-col justify-between border-2 bg-white cursor-default border-bmka-primary-blue rounded'
+                      >
+                          <p className='text-sm text-gray-400'>
+                              ID : {item.id}
+                          </p>
+                          <p className='text-sm text-gray-400'>
+                              {item.created_at}
+                          </p>
 
-                <div className='flex mt-4 justify-between'>
-                    <div>
-                        <h2 className='text-xl'>Masalah Diri Sendiri</h2>
-                        <h4 className='text-bmka-primary-blue'>
-                            Kesehatan Mental
-                        </h4>
-                    </div>
-                    <div className='flex items-center bg-bmka-primary-blue rounded p-2'>
-                        <h4 className='text-white'>Sedang di Tangani</h4>
-                    </div>
-                </div>
-                <div
-                    className='flex justify-between mt-4 border-bmka-accent-orange rounded border-2 p-2'
-                    style={{ width: '21rem' }}
-                >
-                    <div>
-                        <h4 className='text-bmka-accent-orange text-lg'>
-                            Konselor
-                        </h4>
-                    </div>
-                    <div className='flex flex-col justify-center text-gray-500'>
-                        <div className='flex'>
-                            <p className='text-sm w-28'>Penanganan :</p>
-                            <p className='text-sm'>Langsung</p>
-                        </div>
-                        <div className='flex'>
-                            <p className='text-sm w-28'>Jenis Kelamin :</p>
-                            <p className='text-sm'>Perempuan</p>
-                        </div>
-                    </div>
-                </div>
-
-                <p className='mt-4'>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div>
+                          <div className='flex mt-4 justify-between'>
+                              <div>
+                                  <h2 className='text-xl'>
+                                      Masalah {item.problem_owner}
+                                  </h2>
+                                  <h4 className='text-bmka-primary-blue'>
+                                      {item.problem_category}
+                                  </h4>
+                              </div>
+                              <div className='flex items-center bg-bmka-primary-blue rounded p-2'>
+                                  <h4 className='text-white'>
+                                      {item.status_handling}
+                                  </h4>
+                              </div>
+                          </div>
+                          <div
+                              className='flex justify-between mt-4 border-bmka-accent-orange rounded border-2 p-2'
+                              style={{ width: '21rem' }}
+                          >
+                              <div>
+                                  <h4 className='text-bmka-accent-orange text-lg'>
+                                      Konselor
+                                  </h4>
+                              </div>
+                              <div className='flex flex-col justify-center text-gray-500'>
+                                  <div className='flex'>
+                                      <p className='text-sm w-28'>
+                                          Penanganan :
+                                      </p>
+                                      <p className='text-sm'>
+                                          {item.technical_handling}
+                                      </p>
+                                  </div>
+                                  <div className='flex'>
+                                      <p className='text-sm w-28'>
+                                          Jenis Kelamin :
+                                      </p>
+                                      <p className='text-sm'>
+                                          {item.counselor_gender}
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  ))
+                : null}
         </div>
     )
 }
